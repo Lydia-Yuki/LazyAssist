422775e3fddbfa7620c2346895a1a343ac3a0a38
Lydia-Yuki <purple.rose.petalls@gmail.com>
2021-07-17 20:45:36 +0200

    More updates

--- app/src/main/java/com/example/lazyassist/MainActivity.kt
+++ app/src/main/java/com/example/lazyassist/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.lazyassist
 
 import android.app.Activity
 import android.content.ActivityNotFoundException
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.view.accessibility.AccessibilityNodeInfo
+import android.speech.tts.TextToSpeech
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,6 +18,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private val REQUEST_CODE_SPEECH_INPUT = 100
+    lateinit var mTTS: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +29,26 @@ class MainActivity : AppCompatActivity() {
             speak();
         }
         //intent?.handleIntent()
+
+        mTTS = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR) {
+                //if there is no error then set language
+                mTTS.language = Locale.UK
+            }
+            //speak button click
+            speakBtn.setOnClickListener {
+                //get text from edit text
+                val toSpeak = textTv.text.toString()
+                if (toSpeak == "") {
+                    //if there is no text in edit text
+                    Toast.makeText(this, "Enter text", Toast.LENGTH_SHORT).show()
+                } else {
+                    //if there is text in edit text
+                    Toast.makeText(this, toSpeak, Toast.LENGTH_SHORT).show()
+                    mTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+                }
+            }
+        })
     }
 
 //    override fun onNewIntent(intent: Intent?) {
@@ -54,7 +77,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode){
@@ -65,34 +87,34 @@ class MainActivity : AppCompatActivity() {
                     val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
                     //set the text to text view
                     textTv.text = result?.get(0)
-//                    if (textTv.text.contains("call", ignoreCase = true)) {
-//                        val string = textTv.text
-//                        val name = String.substringAfterLast("call")
-//
-//                        val callIntent = Intent().apply {
-//                            action = Intent.ACTION_CALL
-//                            putExtra(Intent.EXTRA_PHONE_NUMBER, name)
-//                            //data = Uri.parse("tel:phoneNumber")
-//
-//
-//                        }
-//
-//                        // Try to invoke the intent.
-//                        try {
-//                            startActivity(callIntent)
-//                        } catch (e: ActivityNotFoundException) {
-//                            // Define what your app should do if no activity can handle the intent.
-//                            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
+                    if (textTv.text.contains("call", ignoreCase = true)) {
+                        //val string = textTv.text
+                        //val name = string.substringAfterLast("call")
 
-                    if (textTv.text.contains("Whatsapp", ignoreCase = true)) {
+                        val callIntent = Intent().apply {
+                            action = Intent.ACTION_CALL
+                            putExtra(Intent.EXTRA_PHONE_NUMBER, "0776631228")
+                            //data = Uri.parse("tel:phoneNumber")
+
+
+                        }
+
+                        // Try to invoke the intent.
+                        try {
+                            startActivity(callIntent)
+                        } catch (e: ActivityNotFoundException) {
+                           // Define what your app should do if no activity can handle the intent.
+                            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    if (textTv.text.contains("home", ignoreCase = true)) {
                         // Create the text message with a string
                         val appIntent = Intent().apply {
-                            val app: Intent = Uri.parse("com.whatsapp").let { appl -> Intent(Intent.ACTION_VIEW, appl) }
+                            //val app: Intent = Uri.parse("com.whatsapp").let { appl -> Intent(Intent.ACTION_VIEW, appl) }
                             action = Intent.ACTION_SEND
                             putExtra(Intent.EXTRA_TEXT, textTv.text)
-                            putExtra(Intent.EXTRA_COMPONENT_NAME, app)
+                            putExtra(Intent.CATEGORY_HOME,"")
                             type = "text/plain"
                         }
 
@@ -106,23 +128,39 @@ class MainActivity : AppCompatActivity() {
                         }
                     }
 
-                    if (textTv.text.contains("text", ignoreCase = true)) {
-                        // Create the text message with a string.
-                        val sendIntent = Intent().apply {
-                            action = Intent.ACTION_SEND
-                            putExtra(Intent.EXTRA_TEXT, textTv.text)
-                            type = "text/plain"
-                        }
+                    if (textTv.text.contains("Duolingo", ignoreCase = true)) {
+                    val intent = Intent(Intent.ACTION_MAIN)
+                    intent.component = ComponentName.unflattenFromString("com.duolingo")
 
-                        // Try to invoke the intent.
-                        try {
-                            startActivity(sendIntent)
-                        } catch (e: ActivityNotFoundException) {
-                            // Define what your app should do if no activity can handle the intent.
-                            Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
-                        }
+
+                    // Try to invoke the intent.
+                    try {
+                        intent.addCategory(Intent.CATEGORY_LAUNCHER)
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        // Define what your app should do if no activity can handle the intent.
+                        Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
                     }
                 }
+
+                if (textTv.text.contains("text", ignoreCase = true)) {
+                    // Create the text message with a string.
+                    val sendIntent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, textTv.text)
+                        putExtra(Intent.CATEGORY_APP_MESSAGING, textTv.text)
+                        type = "text/plain"
+                    }
+
+                    // Try to invoke the intent.
+                    try {
+                        startActivity(sendIntent)
+                    } catch (e: ActivityNotFoundException) {
+                        // Define what your app should do if no activity can handle the intent.
+                        Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
             }
         }
     }
--- app/src/main/java/com/example/lazyassist/MyAccessibilityService.kt
+++ app/src/main/java/com/example/lazyassist/MyAccessibilityService.kt
@@ -4,16 +4,17 @@ package com.example.lazyassist
 import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.os.Build
 import android.os.Bundle
 import android.speech.RecognizerIntent
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.WindowManager
+import android.speech.tts.TextToSpeech
+import android.view.*
 import android.view.accessibility.AccessibilityEvent
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat.startActivityForResult
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -21,7 +22,10 @@ import java.util.*
 class MyAccessibilityService : AccessibilityService() {
     var mLayout: FrameLayout? = null
 
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onServiceConnected() {
+
         // Create an overlay and display the action bar
         val wm = getSystemService(WINDOW_SERVICE) as WindowManager
         mLayout = FrameLayout(this)
@@ -36,16 +40,39 @@ class MyAccessibilityService : AccessibilityService() {
         inflater.inflate(R.layout.activity_main, mLayout)
         wm.addView(mLayout, lp)
 
-
+        //configureButton();
 
     }
 
     override fun onInterrupt() {
 
     }
+    
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
         configureButton();
+
+        lateinit var mTTS: TextToSpeech
+
+        val eventType = event!!.eventType
+        var eventText: String? = null
+        when (eventType) {
+            AccessibilityEvent.TYPE_VIEW_CLICKED -> eventText = "Focused: "
+            AccessibilityEvent.TYPE_VIEW_FOCUSED -> eventText = "Focused: "
+        }
+
+        eventText = eventText + event.contentDescription
+
+        mTTS = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR) {
+                //if there is no error then set language
+                mTTS.language = Locale.UK
+            }
+        })
+
+        mTTS.speak(eventText, TextToSpeech.QUEUE_FLUSH, null)
+
     }
 
     val REQUEST_CODE_SPEECH_INPUT = 100
@@ -55,18 +82,20 @@ class MyAccessibilityService : AccessibilityService() {
         voiceButton.setOnClickListener{
             speak();
             }
+
+
         }
 
     private fun speak() {
         //intent to show speech to text dialogue
         val mIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         mIntent.putExtra(
-            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
         )
         mIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
         mIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something")
-        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        //mIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
 
         val b = Bundle();
         b.putInt("speech input", REQUEST_CODE_SPEECH_INPUT)
@@ -74,6 +103,7 @@ class MyAccessibilityService : AccessibilityService() {
 
         try {
             //if there is no error show speechtotext dialogue
+            mIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
             startActivity(mIntent, b)
         }
         catch (e: Exception){
